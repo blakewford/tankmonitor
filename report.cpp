91fdee0aa9e226a507ed0ddc9c7f7f0cd62366ab
Blake W. Ford <blakewford@gmail.com>
2026-04-12 19:35:10 -0500

    Auto refresh the status page

--- report.cpp
+++ report.cpp
@@ -34,6 +34,9 @@ void create_status_report(float percentage_full, PUMP_STATE state, const char* d
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script>
+    setTimeout(() => { location.reload(); }, 1000);
+  </script>
   <title>Tank Monitor</title>
   <style>
     * {
